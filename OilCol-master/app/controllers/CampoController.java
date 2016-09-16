76961e15caee1996a8922053fc5323ca4b0ec0fa
Julian Camilo Gomez <jc.gomez18@uniandes.edu.co>
2016-09-16 17:35:54 -0500

    caudal y campo ctrl

---
+++ OilCol-master/app/controllers/CampoController.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import dispatchers.AkkaDispatcher;
+import java.util.concurrent.CompletableFuture;
+import static play.libs.Json.toJson;
+import models.ProductEntity;
+import akka.dispatch.MessageDispatcher;
+import play.mvc.*;
+import java.util.concurrent.CompletionStage;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class CampoController  extends Controller
+{
+
+
+    public CompletionStage<Result> getCampos() {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(
+                        () -> {
+                            return CampoEntity.FINDER.all();
+                        }
+                        ,jdbcDispatcher)
+                .thenApply(
+                        campoEntities -> {
+                            return ok(toJson(campoEntities));
+                        }
+                );
+    }
+
+
+
+    public CompletionStage<Result> getCampo(Long id) {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(
+                        () -> {
+
+                            return CampoEntity.FINDER.byId(id);
+                        }
+                        ,jdbcDispatcher)
+                .thenApply(
+                        campoEntities -> {
+                            return ok(toJson(campoEntities));
+                        }
+                );
+    }
+
+
+
+    public CompletionStage<Result> createCampo(){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        JsonNode nCampo = request().body().asJson();
+        CampoEntity campo = Json.fromJson( nCampo , CampoEntity.class ) ;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    campo.save();
+                    return campo;
+                }
+        ).thenApply(
+                campoEntity -> {
+                    return ok(Json.toJson(campoEntity));
+                }
+        );
+    }
+}
---
+++ OilCol-master/app/controllers/CaudalController.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import dispatchers.AkkaDispatcher;
+import java.util.concurrent.CompletableFuture;
+import static play.libs.Json.toJson;
+import models.ProductEntity;
+import akka.dispatch.MessageDispatcher;
+import play.mvc.*;
+import java.util.concurrent.CompletionStage;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class CaudalController  extends Controller
+{
+
+
+    public CompletionStage<Result> getCaudales() {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(
+                        () -> {
+                            return CaudalEntity.FINDER.all();
+                        }
+                        ,jdbcDispatcher)
+                .thenApply(
+                        caudalEntities -> {
+                            return ok(toJson(caudalEntities));
+                        }
+                );
+    }
+
+
+
+    public CompletionStage<Result> getCaudal(Long id) {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(
+                        () -> {
+
+                            return CaudalEntity.FINDER.byId(id);
+                        }
+                        ,jdbcDispatcher)
+                .thenApply(
+                        caudalEntities -> {
+                            return ok(toJson(caudalEntities));
+                        }
+                );
+    }
+
+
+
+    public CompletionStage<Result> createCaudal(){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        JsonNode nCaudal = request().body().asJson();
+        CaudalEntity cau = Json.fromJson( nCaudal , CaudalEntity.class ) ;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    cau.save();
+                    return cau;
+                }
+        ).thenApply(
+                caudalEntity -> {
+                    return ok(Json.toJson(caudalEntity));
+                }
+        );
+    }
+}
